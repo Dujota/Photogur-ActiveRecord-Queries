7b6ba43f4dc491f605f307d79c93887b8b3214d7
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-17 20:04:01 -0400

    created sql queries int he picture model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,15 @@
 class Picture < ApplicationRecord
+
+  def self.newest_first
+    Picture.order("created_at DESC")
+  end
+
+  def self.most_recent_five
+    Picture.newest_first.limit(5)
+  end
+
+  def self.crearted_before(time)
+    Picture.where("created_at < ?", time)
+  end
+
 end
