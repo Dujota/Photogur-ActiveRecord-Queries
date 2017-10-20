d34c2e58cbb5dd388b1962add78ab78aa1ab6b56
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-20 19:01:43 -0400

    made a partial for displaying views accross the picture model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pictures#index'
   get 'pictures/old' => 'pictures#old'
+  get 'pictures/year' => 'pictures#year'
   resources :pictures
 
 
